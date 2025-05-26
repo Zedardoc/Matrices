82aaecdcd2cff063717656022f0f78d20612b048
Javaxst <javier777sb@gmail.com>
2025-05-26 13:03:03 -0500

    Update Vectores.cpp

--- Vectores.cpp
+++ Vectores.cpp
@@ -5,11 +5,12 @@ int i, j, filas, columnas, cam;
 float k;
 
 int main(){
+	///Dimensiones de la matriz:
 	printf("Ingrese las filas que desea:");
 	scanf("%d", &filas);
 	printf("Ingrese las columnas que desea:");
 	scanf("%d", &columnas);
-	
+	///Elementos de la matriz
 	float m1[filas][columnas], m2[filas][columnas];
 	for(i=0;i<filas;i++){
 		for(j=0;j<columnas;j++){
@@ -17,33 +18,35 @@ int main(){
 			scanf("%f", &m1[i][j]);
 		}
 	}
+	///Impresión de la matriz
 	for(i=0;i<filas;i++){
 		for(j=0;j<columnas;j++){
 			printf("%.2f ", m1[i][j]);
 		}
 		printf("\n");
 	}
-	printf("\nIngrese el numero de columna a modificar [1]-[%d]: ", columnas);
+	printf("\nIngrese el numero de columna a modificar [1]-[%d]: ", columnas);///El Usuario decide la columna a cambiar
     scanf("%d", &cam);
 	cam--;
-    printf("Ingrese el escalar:");
+    printf("Ingrese el escalar:");///escoge el factor por el cual se multiplicará
     scanf("%f", &k);
     
     for(i=0;i<filas;i++){
     	for(j=0;j<columnas;j++){
-    		if(j==cam){
-    			m2[i][j]=m1[i][j]*k;
+    		if(j==cam){///Este if permite que el factor solo multiplique a la columna que escoja el usuario
+    			m2[i][j]=m1[i][j]*k;///Declaración de la nueva matriz
 			}
 			else{
-				m2[i][j]=m1[i][j];
+				m2[i][j]=m1[i][j];///Si la columna que escoge el usuario no existe, entonces se imprime la matriz original
 			}
 		}
 	}
+	///impresión de la nueva matriz
 	for(i=0;i<filas;i++){
 		for(j=0;j<columnas;j++){
 			printf("%.2f ", m2[i][j]);
 		}
-		printf("\n");
+		printf("\n");///este print es escencial para que quede como una matriz, ya que si no se hiciese el salto de línea, todo queda en la misma línea
 	}
 	return 0;
 }
