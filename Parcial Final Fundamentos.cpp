07d924ef9f5a3f1fa7d5246c7a8bd5240761e643
Samuel Oñate <samuelonate0511@gmail.com>
2025-06-02 01:23:01 -0500

    Terminado, solo falta poner comentarios

--- Parcial Final Fundamentos.cpp
+++ Parcial Final Fundamentos.cpp
@@ -4,7 +4,7 @@
 
 
 int Estu, i, m=0, f=0, j, gen, opcion;
- int main(){
+int main(){
  	printf("Bienvenido, promedio de notas\n");
  		while(1){	
  			printf("Ingrese la cantidad de alumnos:\n");
@@ -45,24 +45,24 @@ int Estu, i, m=0, f=0, j, gen, opcion;
 	 			scanf("%d", &gen);
 		 			if(gen==0 || gen==1){
 		 				if(gen==0){
-		 					f++;
-					 }
+							f++;
+						}
 					 	else{
 					 		m++;
-					 }
+					 	}
 		 			break;
 					}
 					else{
-					printf("Opcion no coincide\n");
-					}
-				 	}
+						printf("Opcion no coincide\n");
+				}
 			}
+		}
 		printf("Ingrese las notas de algebra de cada alumno:\n");
 		 for(i=0;i<Estu;i++){
 		 	while(1){
 		 		printf("Alumno[%d]:", i+1);
-				scanf("%f", &NotasA[Estu]); 
-					if(NotasA[Estu]<0 || NotasA[Estu]>5){
+				scanf("%f", &NotasA[i]); 
+					if(NotasA[i]<0 || NotasA[i]>5){
 						printf("Aqui calificamos de 0 a 5");
 					}
 					else{
@@ -74,9 +74,9 @@ int Estu, i, m=0, f=0, j, gen, opcion;
 		 for(i=0;i<Estu;i++){
 		 	while(1){
 		 		printf("Alumno[%d]:", i+1);
-				scanf("%f", &NotasF[Estu]); 
-					if(NotasF[Estu]<0 || NotasF[Estu]>5){
-						printf("Aqui calificamos de 0 a 5\n");
+				scanf("%f", &NotasF[i]); 
+					if(NotasF[i]<0 || NotasF[i]>5){
+						printf("Aqui calificamos de 0 a 5 \n");
 					}
 					else{
 						break;
@@ -85,48 +85,68 @@ int Estu, i, m=0, f=0, j, gen, opcion;
 		 }
 	 printf("Cantidad de alumnos: %d\n", Estu);
 		 for(i=0;i<Estu;i++){
-		 	printf("%d. %d	%d  %3.f  %3.f\n", i+1, Codigos[i], gen[i], NotasA[i], NotasF[i]);
+		 	printf("%d %d %.1f  %.1f\n", i+1, Codigos[i], NotasA[i], NotasF[i]);
 		 }
-	 printf("Menu, escoja alguna de las siguientes opciones:\n");
-	 printf("1. Promedio de Algebra:\n");
-	 printf("2. Mayor Nota de fundamentos de programacion:\n");
-	 printf("3. Cantidad de Alumnos Femeninos y Masculinos:\n");
-	 printf("5. Buscar Nota mediante codigo:\n");
-	 scanf("%d", &opcion);
-	 switch(opcion){
-	 	case 1:{
-	 		
-			break;
-		 }
-		case 2:{
+		 
+	//Imprimir el menu y datos
+	while(1){
+		 printf("Menu, escoja alguna de las siguientes opciones:\n");
+		 printf("1. Promedio de Algebra:\n");
+		 printf("2. Mayor Nota de fundamentos de programacion:\n");
+		 printf("3. Cantidad de Alumnos Femeninos y Masculinos:\n");
+		 printf("4. Buscar Nota mediante codigo:\n");
+		 printf("5. Salir\n");
+		 scanf("%d", &opcion);
+		 switch(opcion){
+		 	case 1:
+		 		float promedio;
+		 		for(int i = 0; i<Estu; i++){
+		 			promedio += NotasA[i];
+				 }
+				 promedio = promedio/Estu;
+				 printf("El promedio de algebra de todos los estudiantes fue de %.1f\n\n", promedio);
+				break;
+			 
+			case 2:
+				float nota;
+				nota=0;
+				for(int i = 0; i<Estu;i++){
+					if(NotasF[i]>nota){
+						nota = NotasF[i];
+					}
+				}
+				printf("La mayor nota alcanzada en fundamentos fue %.1f\n\n", nota);
+				break;
+			case 3:
+				printf("Femeninos: %d - Masculinos: %d\n\n", f, m);
+				break;
 			
-			break;
-		}
-		case 3:{
+			case 4:
+				int codigo;
+				int encontrado;
+				encontrado = 0;
+				printf("Especifique el codigo deseado: \n");
+				scanf("%d", &codigo);
+				for(int i=0;i<Estu; i++){
+					if(codigo==Codigos[i]){
+						printf("El codigo: %d - Algebra: %.1f - Fundamentos: %.1f\n", codigo, NotasA[i], NotasF[i]);
+						encontrado = 1;
+						break;
+					}
+				}
+				if(encontrado == 0){
+					printf("No se ha encontrado un alumno con ese codigo\n");	
+				}
+				break;
 			
-			break;
-		}
-		case 4:{
+			case 5:
+				return 1;
+				break;
 			
-			break;
-		}
-		case 5:{
+			default:
+				printf("Opcion no existe\n");
+				break;
 			
-			break;
 		}
-		default:{
-			printf("Opcion no existe");
-			break;
-		}
-	 
-}
-	
- 	
- 	
- 	
- 	
- 	
- 	
- 	
- 	return 0;
+	}
  }
